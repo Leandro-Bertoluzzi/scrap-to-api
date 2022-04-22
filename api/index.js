291a1387246770c250be6684c0089d9173db8154
Leandro Bertoluzzi <leajb10@gmail.com>
2022-04-21 23:12:05 -0300

    feat: Added Puppeteer and simple web scraping example

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const express = require("express");
-var cors = require('cors');
 const config = require("./config/config");
+const browserObject = require('./services/browser');
+const scraperController = require('./services/pageController');
 
 // Constants
 const API_PORT = config.API_PORT;
@@ -10,9 +11,6 @@ const API_HOST = config.API_HOST;
 
 // Define app
 const app = express();
-
-// Middleware for allowing CORS
-app.use(cors());
 
 // ******************** API ******************** //
 
@@ -24,4 +22,9 @@ app.get("/", (req, res) => {
 // Validate connection
 app.listen(API_PORT, API_HOST, function(){
   console.log(`Server running on http://${API_HOST}:${API_PORT}`);
+
+  //Start the browser and create a browser instance
+  let browserInstance = browserObject.startBrowser();
+  // Pass the browser instance to the scraper controller
+  scraperController(browserInstance);
 });
