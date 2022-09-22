c3efa0b03606e3b2bdbb0c813d8485e61aeb9893
Leandro Bertoluzzi <leajb10@gmail.com>
2022-09-21 21:41:18 -0300

    refactor: Refactored search routes file to use async/await

--- api/routes/search.js
+++ api/routes/search.js
@@ -7,7 +7,7 @@ const pageScraper = require('../services/pageScraper');
 router.use(express.json());
 
 // Searches an anime/manga/character/people/company
-router.get("/:type", function (req, res) {
+router.get("/:type", async function (req, res) {
   // Get type from URL params
   let type = req.params.type || null;
   if(!type){
@@ -17,23 +17,18 @@ router.get("/:type", function (req, res) {
   let searchQuery = req.query.query || '';
   let currentPage = req.query.page || 0;
   
-  //Start the browser and create a browser instance
-  startBrowser().then(function (browser) {
-    pageScraper.search(browser, type, searchQuery, currentPage).then(function (data){
-      var values = JSON.parse(JSON.stringify(data));
-      res.status(200).json({result: values});
-    }).catch(function (error) {
-      console.log("There was an error (1) => ", error);
-      var errorMessage = JSON.parse(JSON.stringify(error.message));
-      res.status(400).json({error: errorMessage});
-    }).finally(() => {
-      browser.close();
-    });
-  }).catch(function (error) {
-		console.log("There was an error (2) => ", error);
+  try {
+    const browser = await startBrowser();
+    const data = await pageScraper.search(browser, type, searchQuery, currentPage);
+    browser.close();
+
+    var values = JSON.parse(JSON.stringify(data));
+    res.status(200).json({result: values});
+  } catch(error) {
+		console.log("There was an error: ", error);
     var errorMessage = JSON.parse(JSON.stringify(error.message));
     res.status(400).json({error: errorMessage});
-  });
+  }
 });
 
 module.exports = router;
